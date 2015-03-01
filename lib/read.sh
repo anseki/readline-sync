c33a9d77cd150c3bb9bf17a4267e5d2e7245374e
anseki <AnSeki.aff@gmail.com>
2015-03-02 02:00:20 +0900

    Drop STDERR of `stty`.

--- lib/read.sh
+++ lib/read.sh
@@ -1,7 +1,12 @@
 if [ "$1" = "noechoback" ]; then
-  stty --file=/dev/tty -echo echonl || stty -F /dev/tty -echo echonl || stty -f /dev/tty -echo echonl || exit 1
+  stty --file=/dev/tty -echo echonl 2>/dev/null || \
+    stty -F /dev/tty -echo echonl 2>/dev/null || \
+    stty -f /dev/tty -echo echonl 2>/dev/null || \
+    exit 1
   IFS= read -r INPUT </dev/tty
-  stty --file=/dev/tty echo -echonl || stty -F /dev/tty echo -echonl || stty -f /dev/tty echo -echonl
+  stty --file=/dev/tty echo -echonl 2>/dev/null || \
+    stty -F /dev/tty echo -echonl 2>/dev/null || \
+    stty -f /dev/tty echo -echonl 2>/dev/null
   # printf '\n' >/dev/tty
 else
   IFS= read -r INPUT </dev/tty
