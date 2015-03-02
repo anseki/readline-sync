9e8a318880ab71e0f233fb47e6284e2207f3dcd5
anseki <AnSeki.aff@gmail.com>
2015-03-02 22:44:36 +0900

    fixed #13 Error on Node v0.11+

--- lib/read.sh
+++ lib/read.sh
@@ -1,29 +1,24 @@
 silentRead() {
-  # `typeset` doesn't work in `func()` of ksh.
-  # `function fnc` for local-var of ksh is not compatible.
-  # Therefore, `_input` is not local-var on ksh.
-  local _input="" 2>/dev/null || typeset _input=""
   stty --file=/dev/tty -echo echonl 2>/dev/null || \
     stty -F /dev/tty -echo echonl 2>/dev/null || \
     stty -f /dev/tty -echo echonl 2>/dev/null || \
     exit 1
-  IFS= read -r _input </dev/tty
+  IFS= read -r INPUT </dev/tty
   stty --file=/dev/tty echo -echonl 2>/dev/null || \
     stty -F /dev/tty echo -echonl 2>/dev/null || \
     stty -f /dev/tty echo -echonl 2>/dev/null
-  printf '%s' "$_input"
 }
 
 if [ "$1" = "noechoback" ]; then
   # Try `-s` option. *ksh have it that not `--silent`. Therefore, don't try it.
   if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    IFS= read -rs INPUT </dev/tty 2>/dev/null || INPUT=`silentRead` || exit 1
+    IFS= read -rs INPUT </dev/tty 2>/dev/null || silentRead
     printf '\n' >/dev/tty
   else
-    INPUT=`silentRead` || exit 1
+    silentRead
   fi
 else
-  IFS= read -r INPUT </dev/tty
+  IFS= read -r INPUT </dev/tty 2>/dev/null || exit 1
 fi
 printf '%s' "'$INPUT'"
 exit 0
