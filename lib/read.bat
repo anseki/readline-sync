fc50c9636817fa6242d5003499d67f6534d7d496
anseki <AnSeki.aff@gmail.com>
2015-03-04 02:30:41 +0900

    Fix: Can't get error from shell

--- lib/read.bat
+++ lib/read.bat
@@ -1,9 +1,13 @@
 @echo off
 setlocal
+setlocal ENABLEDELAYEDEXPANSION
+
 if "%1"=="noechoback" (
   call :exprog
+  if ERRORLEVEL 1 exit /b 1
 ) else (
   set /p INPUT=<CON >CON
+  if ERRORLEVEL 1 exit /b 1
 )
 set /p ="'%INPUT%'"<NUL
 endlocal
@@ -12,19 +16,27 @@ exit /b 0
 :exprog
 
 :: where /q powershell
-:: Win <Vista and <Server2008 don't have `WHERE`.
+:: Win <Vista and <Server2008 don't have `where`.
 powershell /? >NUL 2>&1
-
 :: Win <7 and <Server2008R2 don't have PowerShell as default.
 :: Win XP and Server2003 have `ScriptPW` (`scriptpw.dll`).
 :: In the systems that don't have both, an error is thrown.
-if errorlevel 1 (
-  set "EXCOMMAND=cscript //nologo "%~dp0read.cs.js""
+if ERRORLEVEL 1 (
+  set "EXECOMMAND=cscript //nologo "%~dp0read.cs.js""
 ) else (
-  set "EXCOMMAND=powershell -Command "$text = read-host -AsSecureString; ^
+  set "EXECOMMAND=powershell -Command "$text = read-host -AsSecureString; ^
     $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR^($text^); ^
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto^($BSTR^)""
 )
-:: echo %EXCOMMAND%
-for /f "usebackq delims=" %%i in (`%EXCOMMAND%`) do set "INPUT=%%i"
-exit /b
+
+:: Can't get `ERRORLEVEL` from sub-shell (`for`).
+:: 2 `%ERRCODE%` lines are returned if an error is thrown.
+set ERRCODE=ERR
+set "EXECOMMAND=%EXECOMMAND% ^& if ERRORLEVEL 1 ^(echo %ERRCODE%^& echo %ERRCODE%^)"
+:: echo %EXECOMMAND%
+
+for /f "usebackq delims=" %%i in (`%EXECOMMAND%`) do (
+  if "%%i"=="%ERRCODE%" if "!INPUT!"=="%ERRCODE%" exit /b 1
+  set "INPUT=%%i"
+)
+exit /b 0
--- lib/read.sh
+++ lib/read.sh
@@ -1,24 +1,23 @@
-silentRead() {
+silent_read() {
   stty --file=/dev/tty -echo echonl 2>/dev/null || \
     stty -F /dev/tty -echo echonl 2>/dev/null || \
-    stty -f /dev/tty -echo echonl 2>/dev/null || \
-    exit 1
-  IFS= read -r INPUT </dev/tty
+    stty -f /dev/tty -echo echonl || exit 1
+  IFS= read -r INPUT </dev/tty || exit 1
   stty --file=/dev/tty echo -echonl 2>/dev/null || \
     stty -F /dev/tty echo -echonl 2>/dev/null || \
-    stty -f /dev/tty echo -echonl 2>/dev/null
+    stty -f /dev/tty echo -echonl || exit 1
 }
 
 if [ "$1" = "noechoback" ]; then
   # Try `-s` option. *ksh have it that not `--silent`. Therefore, don't try it.
   if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    IFS= read -rs INPUT </dev/tty 2>/dev/null || silentRead
+    IFS= read -rs INPUT </dev/tty 2>/dev/null || silent_read
     printf '\n' >/dev/tty
   else
-    silentRead
+    silent_read
   fi
 else
-  IFS= read -r INPUT </dev/tty 2>/dev/null || exit 1
+  IFS= read -r INPUT </dev/tty || exit 1
 fi
 printf '%s' "'$INPUT'"
 exit 0
