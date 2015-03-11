16c0c476cdcf99863712608b3341692c3d02a7d6
anseki <AnSeki.aff@gmail.com>
2015-03-11 17:06:27 +0900

    Add `display` to shell script.

--- lib/read.bat
+++ lib/read.bat
@@ -1,14 +1,39 @@
 @echo off
-setlocal
 setlocal ENABLEDELAYEDEXPANSION
 
-if "%~1"=="noechoback" (
+:args_loop
+if "%~1"=="" (
+  goto args_end
+
+) else if "%~1"=="--noechoback" (
+  set noechoback=1
+
+) else if "%~1"=="--keyin" (
+  set keyin=1
+
+) else if "%~1"=="--display" (
+  set "display=%~2"
+  shift /1
+
+)
+shift /1
+goto args_loop
+:args_end
+
+:: type tmpfile.txt >CON
+if "%display%" NEQ "" if "%NODE_EXEC_PATH%" NEQ "" (
+  "%NODE_EXEC_PATH%" "%~dp0decodedos.js" "%display%" >CON
+  if ERRORLEVEL 1 exit /b 1
+)
+
+if "%noechoback%"=="1" (
   call :read_s
   if ERRORLEVEL 1 exit /b 1
 ) else (
-  set /p INPUT=<CON >CON
+  set /p input=<CON >CON
 )
-set /p ="'%INPUT%'"<NUL
+set /p ="'%input%'"<NUL
+
 endlocal
 exit /b 0
 
@@ -22,21 +47,22 @@ powershell /? >NUL 2>&1
 :: Win XP and Server2003 have `ScriptPW` (`scriptpw.dll`).
 :: In the systems that don't have both, an error is thrown.
 if ERRORLEVEL 1 (
-  set "EXECOMMAND=cscript //nologo "%~dp0read.cs.js""
+  set "exec_line=cscript //nologo "%~dp0read.cs.js""
 ) else (
-  set "EXECOMMAND=powershell -Command "$text = read-host -AsSecureString; ^
+  set "exec_line=powershell -Command "$text = read-host -AsSecureString; ^
     $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR^($text^); ^
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto^($BSTR^)""
 )
 
-:: Can't get `ERRORLEVEL` from sub-shell (`for`).
+:: Can't get `ERRORLEVEL` from sub-shell.
 :: 2 `%ERRCODE%` lines are returned if an error is thrown.
 set ERRCODE=ERR
-set "EXECOMMAND=%EXECOMMAND% ^& if ERRORLEVEL 1 ^(echo %ERRCODE%^& echo %ERRCODE%^)"
-:: echo %EXECOMMAND%
+set "exec_line=%exec_line% ^& if ERRORLEVEL 1 ^(echo %ERRCODE%^& echo %ERRCODE%^)"
+:: echo %exec_line%
 
-for /f "usebackq delims=" %%i in (`%EXECOMMAND%`) do (
-  if "%%i"=="%ERRCODE%" if "!INPUT!"=="%ERRCODE%" exit /b 1
-  set "INPUT=%%i"
+for /f "usebackq delims=" %%i in (`%exec_line%`) do (
+  if "%%i"=="%ERRCODE%" if "!input!"=="%ERRCODE%" exit /b 1
+  set "input=%%i"
 )
+
 exit /b 0
--- lib/read.sh
+++ lib/read.sh
@@ -3,21 +3,36 @@ read_s() {
   stty --file=/dev/tty -echo echonl 2>/dev/null || \
     stty -F /dev/tty -echo echonl 2>/dev/null || \
     stty -f /dev/tty -echo echonl || exit 1
-  IFS= read -r INPUT </dev/tty || exit 1
+  IFS= read -r input </dev/tty || exit 1
   stty --file=/dev/tty echo -echonl 2>/dev/null || \
     stty -F /dev/tty echo -echonl 2>/dev/null || \
     stty -f /dev/tty echo -echonl || exit 1
 }
 
-if [ "$1" = "noechoback" ]; then
+# getopt(s)
+while [ $# -ge 1 ]; do
+  case "$1" in
+    "--noechoback")     noechoback=1;;
+    "--keyin")          keyin=1;;
+    "--display")        shift; display=$1;;
+  esac
+  shift
+done
+
+if [ -n "$display" ]; then
+  printf '%s' "$display" >/dev/tty
+fi
+
+if [ "$noechoback" = "1" ]; then
   # Try `-s` option. *ksh have it that not `--silent`. Therefore, don't try it.
   if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    IFS= read -rs INPUT </dev/tty 2>/dev/null && printf '\n' >/dev/tty || read_s
+    IFS= read -rs input </dev/tty 2>/dev/null && printf '\n' >/dev/tty || read_s
   else
     read_s
   fi
 else
-  IFS= read -r INPUT </dev/tty || exit 1
+  IFS= read -r input </dev/tty || exit 1
 fi
-printf '%s' "'$INPUT'"
+printf '%s' "'$input'"
+
 exit 0
