67d20da312cccec68ed33c9ef4296b98625e0cb6
anseki <AnSeki.aff@gmail.com>
2015-03-27 20:25:59 +0900

    Rewrite code that call external programs.

--- lib/read.ps1
+++ lib/read.ps1
@@ -2,7 +2,7 @@
 Param(
   [string] $display,
   [switch] $noEchoBack,
-  [string] $mask = '*',
+  [string] $mask,
   [switch] $keyIn,
   [switch] $encoded
 )
@@ -14,24 +14,35 @@ trap {
   exit 1
 }
 
-function decodeDOS($arg) {
+function decodeDOS ($arg) {
   [Regex]::Replace($arg, '#(\d+);', { [char][int] $args[0].Groups[1].Value })
 }
 
-if ($encoded) {
-  if ($display -ne '') { $display = decodeDOS $display }
-  if ($mask -ne '') { $mask = decodeDOS $mask }
+$options = @{}
+foreach ($arg in @('display', 'noEchoBack', 'mask', 'keyIn', 'encoded')) {
+  $options.Add($arg, (Get-Variable $arg -ValueOnly))
+}
+if ($options.encoded) {
+  $argList = New-Object string[] $options.Keys.Count
+  $options.Keys.CopyTo($argList, 0);
+  foreach ($arg in $argList) {
+    if (($options[$arg] -is [string]) -and ($options[$arg] -ne ''))
+      { $options[$arg] = decodeDOS $options[$arg] }
+  }
 }
 
-Write-Warning "[PS] display:     <$display>" #_DBG
-Write-Warning "[PS] noEchoBack:  $noEchoBack" #_DBG
-Write-Warning "[PS] mask:        <$mask>" #_DBG
-Write-Warning "[PS] keyIn:       $keyIn" #_DBG
+[string] $inputTTY = ''
+[bool] $isInputLine = $False
+[bool] $isEditable = (-not $options.noEchoBack) -and (-not $options.keyIn)
+
+function writeTTY ($text) {
+  execWithTTY ('Write-Host ''' + ($text -replace '''', '''''') + ''' -NoNewline')
+  $script:isInputLine = $True
+}
 
 # Instant method that opens TTY without CreateFile via P/Invoke in .NET Framework
-# **NOTE** Don't include special characters in $command when $getRes is True.
+# **NOTE** Don't include special characters of DOS in $command when $getRes is True.
 function execWithTTY ($command, $getRes = $False) {
-  Write-Warning "[PS] command: <$command> getRes: $getRes" #_DBG
   if ($getRes) {
     $res = (cmd.exe /C "<CON powershell.exe -Command $command")
     if ($LastExitCode -ne 0) { exit 1 }
@@ -43,21 +54,19 @@ function execWithTTY ($command, $getRes = $False) {
   return $res
 }
 
-function writeTTY ($text) {
-  execWithTTY ('Write-Host ''' + ($text -replace '''', '''''') + ''' -NoNewline')
+if ($options.display -ne '') {
+  writeTTY $options.display
+  $options.display = ''
 }
 
-[string] $inputTTY = ''
-
-if ($noEchoBack -and (-not $keyIn) -and ($mask -eq '*')) {
+if ($options.noEchoBack -and (-not $options.keyIn) -and ($options.mask -eq '*')) {
   $inputTTY = execWithTTY ('$inputTTY = Read-Host -AsSecureString;' +
     '$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($inputTTY);' +
     '[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)') $True
-  return $inputTTY
+  return '''' + $inputTTY + ''''
 }
 
-$isEditable = (-not $noEchoBack) -and (-not $keyIn)
-if ($keyIn) { $reqSize = 1 }
+if ($options.keyIn) { $reqSize = 1 }
 else { $reqSize = 1024 } # dummy
 
 while ($True) {
@@ -69,25 +78,27 @@ while ($True) {
   }
 
   if ($chunk -eq '') { break }
+  # other ctrl-chars
   $chunk = $chunk -replace '[\x00-\x08\x0b\x0c\x0e-\x1f\x7f]', ''
   if ($chunk -eq '') { continue }
 
   if (-not $isEditable) {
-    $displayInput = $chunk -replace '[\r\n]', ''
-    if ($displayInput -ne '') {
-      if ($noEchoBack) {
-        if ($mask -eq '') { $displayInput = '' }
-        else { $displayInput = $displayInput -replace '.', $mask }
+    $displayTmp = $chunk -replace '[\r\n]', ''
+    if ($displayTmp -ne '') {
+      if ($options.noEchoBack) {
+        if ($options.mask -eq '') { $displayTmp = '' }
+        else { $displayTmp = $options.mask * $displayTmp.Length }
       }
-      if ($displayInput -ne '') { writeTTY $displayInput }
+      if ($displayTmp -ne '') { writeTTY $displayTmp }
     }
   }
 
   $inputTTY += $chunk
-  if (($inputTTY -match '[\r\n]$') -or ($keyIn -and ($inputTTY.Length -ge $reqSize)))
-    { break }
+  if (($inputTTY -match '[\r\n]$') -or
+    ($options.keyIn -and ($inputTTY.Length -ge $reqSize))) { break }
 }
 
-if (-not $isEditable) { writeTTY "`n" } # new-line
+if ((-not $isEditable) -and (-not ($options.keyIn -and (-not $isInputLine))))
+  { execWithTTY 'Write-Host ''''' } # new line
 
-return '''' + $inputTTY + '''' #DBG
+return '''' + $inputTTY + ''''
